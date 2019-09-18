012dafea9d07a53c90857a965915dab4443efbb9
gayankuruppu <example@example.com>
2019-09-18 18:17:36 +0530

    Update Bounce

--- androidrenderanimations/src/main/java/render/animations/Bounce.java
+++ androidrenderanimations/src/main/java/render/animations/Bounce.java
@@ -8,26 +8,30 @@ public class Bounce {
     public static AnimatorSet In(View view) {
         AnimatorSet animatorSet = new AnimatorSet();
 
-        ObjectAnimator object1 = ObjectAnimator.ofFloat(view, "translationY", 0, 0, -30, 0, -15, 0, 0);
+        ObjectAnimator object1 = ObjectAnimator.ofFloat(view, "alpha", 0, 1, 1, 1);
+        ObjectAnimator object2 = ObjectAnimator.ofFloat(view, "scaleX", 0.3f, 1.05f, 0.9f, 1);
+        ObjectAnimator object3 = ObjectAnimator.ofFloat(view, "scaleY", 0.3f, 1.05f, 0.9f, 1);
 
-        animatorSet.playTogether(object1);
+        animatorSet.playTogether(object1, object2, object3);
         return animatorSet;
     }
 
     public static AnimatorSet InDown(View view){
         AnimatorSet animatorSet = new AnimatorSet();
+        long height = - view.getHeight();
+        ObjectAnimator object1 =   ObjectAnimator.ofFloat(view,  "alpha", 0, 1, 1, 1);
+        ObjectAnimator object2 =   ObjectAnimator.ofFloat(view,  "translationY", height, 30, -10, 0);
 
-        ObjectAnimator object =   ObjectAnimator.ofFloat(view, "alpha", 1, 0, 1, 0, 1);
-
-        animatorSet.playTogether(object);
+        animatorSet.playTogether(object1, object2);
         return animatorSet;
     }
 
     public static AnimatorSet InLeft(View view){
         AnimatorSet animatorSet = new AnimatorSet();
+        long width =  - view.getWidth();
 
-        ObjectAnimator object1 = ObjectAnimator.ofFloat(view, "scaleY", 1, 1.1f, 1);
-        ObjectAnimator object2 = ObjectAnimator.ofFloat(view, "scaleX", 1, 1.1f, 1);
+        ObjectAnimator object1 = ObjectAnimator.ofFloat(view, "translationX", width, 30, -10, 0);
+        ObjectAnimator object2 = ObjectAnimator.ofFloat(view, "alpha", 0, 1, 1, 1);
 
         animatorSet.playTogether(object1, object2);
         return animatorSet;
