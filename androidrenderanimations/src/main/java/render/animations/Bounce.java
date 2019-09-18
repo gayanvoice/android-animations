eb9e2e4ad3608e05e6b5e1105f18cdbd21649ae8
gayankuruppu <example@example.com>
2019-09-18 18:12:07 +0530

    Update Bounce

---
+++ androidrenderanimations/src/main/java/render/animations/Bounce.java
@@ -0,0 +1,55 @@
+package render.animations;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.view.View;
+
+public class Bounce {
+    public static AnimatorSet In(View view) {
+        AnimatorSet animatorSet = new AnimatorSet();
+
+        ObjectAnimator object1 = ObjectAnimator.ofFloat(view, "translationY", 0, 0, -30, 0, -15, 0, 0);
+
+        animatorSet.playTogether(object1);
+        return animatorSet;
+    }
+
+    public static AnimatorSet InDown(View view){
+        AnimatorSet animatorSet = new AnimatorSet();
+
+        ObjectAnimator object =   ObjectAnimator.ofFloat(view, "alpha", 1, 0, 1, 0, 1);
+
+        animatorSet.playTogether(object);
+        return animatorSet;
+    }
+
+    public static AnimatorSet InLeft(View view){
+        AnimatorSet animatorSet = new AnimatorSet();
+
+        ObjectAnimator object1 = ObjectAnimator.ofFloat(view, "scaleY", 1, 1.1f, 1);
+        ObjectAnimator object2 = ObjectAnimator.ofFloat(view, "scaleX", 1, 1.1f, 1);
+
+        animatorSet.playTogether(object1, object2);
+        return animatorSet;
+    }
+
+    public static AnimatorSet InRight(View view){
+        AnimatorSet animatorSet = new AnimatorSet();
+
+        ObjectAnimator object1 = ObjectAnimator.ofFloat(view,  "scaleX", 1, 1.25f, 0.75f, 1.15f, 1);
+        ObjectAnimator object2 = ObjectAnimator.ofFloat(view,  "scaleY", 1, 0.75f, 1.25f, 0.85f, 1);
+
+        animatorSet.playTogether(object1, object2);
+        return animatorSet;
+    }
+
+    public static AnimatorSet InUp(View view){
+        AnimatorSet animatorSet = new AnimatorSet();
+
+        ObjectAnimator object = ObjectAnimator.ofFloat(view,   "translationX", 0, 25, -25, 25, -25, 15, -15, 6, -6, 0);
+
+        animatorSet.playTogether(object);
+        return animatorSet;
+    }
+
+}
