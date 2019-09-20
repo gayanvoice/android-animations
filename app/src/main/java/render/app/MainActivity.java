f204040e7a4efff1b04f8e2500a74712ebc0b127
gayankuruppu <example@example.com>
2019-09-20 12:30:43 +0530

    Update App

--- app/src/main/java/render/app/MainActivity.java
+++ app/src/main/java/render/app/MainActivity.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 
 import render.animations.Attention;
 import render.animations.Bounce;
+import render.animations.Fade;
+import render.animations.Flip;
 import render.animations.Render;
+import render.animations.Rotate;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
     Render render;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +39,42 @@ public class MainActivity extends AppCompatActivity {
         Button buttonBounceInDown = findViewById(R.id.btn_bounce_indown);
         Button buttonBounceIn = findViewById(R.id.btn_bounce_in);
 
-        final ImageView imageView = findViewById(R.id.iv_image);
+        Button buttonFadeInUp = findViewById(R.id.btn_fade_inup);
+        Button buttonFadeInDown = findViewById(R.id.btn_fade_indown);
+        Button buttonFadeInRight = findViewById(R.id.btn_fade_inright);
+        Button buttonFadeInLeft = findViewById(R.id.btn_fade_inleft);
+
+        Button buttonFadeOutUp = findViewById(R.id.btn_fade_outup);
+        Button buttonFadeOutDown = findViewById(R.id.btn_fade_outdown);
+        Button buttonFadeOutRight = findViewById(R.id.btn_fade_outright);
+        Button buttonFadeOutLeft = findViewById(R.id.btn_fade_outleft);
+
+        Button buttonFadeIn = findViewById(R.id.btn_fade_in);
+        Button buttonFadeOut = findViewById(R.id.btn_fade_out);
+
+        Button buttonFlipInX = findViewById(R.id.btn_flip_inx);
+        Button buttonFlipInY = findViewById(R.id.btn_flip_iny);
+        Button buttonFlipOutX = findViewById(R.id.btn_flip_outx);
+        Button buttonFlipOutY = findViewById(R.id.btn_flip_outy);
+
+        Button buttonRotateInDownLeft = findViewById(R.id.btn_rotate_indownleft);
+        Button buttonRotateInDownRight = findViewById(R.id.btn_rotate_indownright);
+        Button buttonRotateInUpLeft = findViewById(R.id.btn_rotate_inupleft);
+        Button buttonRotateInUpRight = findViewById(R.id.btn_rotate_inupright);
+        Button buttonRotateOutDownLeft = findViewById(R.id.btn_rotate_outdownleft);
+        Button buttonRotateOutDownRight = findViewById(R.id.btn_rotate_outdownright);
+        Button buttonRotateOutUpLeft = findViewById(R.id.btn_rotate_outupleft);
+        Button buttonRotateOutUpRight = findViewById(R.id.btn_rotate_outupright);
+
+        Button buttonRotateIn = findViewById(R.id.btn_rotate_in);
+        Button buttonRotateOut = findViewById(R.id.btn_rotate_out);
+        imageView = findViewById(R.id.iv_image);
+
 
         render = new Render(this);
         render.setDuration(1000);
+
+
 
 
         buttonAttentionBounce.setOnClickListener(new View.OnClickListener() {
@@ -144,6 +181,176 @@ public class MainActivity extends AppCompatActivity {
                 render.start();
             }
         });
+
+        buttonFadeInUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Fade.InUp(imageView));
+                render.start();
+            }
+        });
+
+        buttonFadeInDown.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Fade.InDown(imageView));
+                render.start();
+            }
+        });
+
+        buttonFadeInRight.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Fade.InRight(imageView));
+                render.start();
+            }
+        });
+
+        buttonFadeInLeft.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Fade.InLeft(imageView));
+                render.start();
+            }
+        });
+
+        buttonFadeOutUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Fade.OutUp(imageView));
+                render.start();
+            }
+        });
+
+
+        buttonFadeOutDown.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Fade.OutDown(imageView));
+                render.start();
+            }
+        });
+
+        buttonFadeOutRight.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Fade.OutRight(imageView));
+                render.start();
+            }
+        });
+
+        buttonFadeOutLeft.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Fade.OutLeft(imageView));
+                render.start();
+            }
+        });
+
+        buttonFadeIn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Fade.In(imageView));
+                render.start();
+            }
+        });
+
+        buttonFadeOut.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Fade.Out(imageView));
+                render.start();
+            }
+        });
+
+        buttonFlipInX.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Flip.InX(imageView));
+                render.start();
+            }
+        });
+
+        buttonFlipInY.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Flip.InY(imageView));
+                render.start();
+            }
+        });
+
+        buttonFlipOutX.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Flip.OutX(imageView));
+                render.start();
+            }
+        });
+
+        buttonFlipOutY.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Flip.OutY(imageView));
+                render.start();
+            }
+        });
+
+        buttonRotateInDownLeft.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Rotate.InDownLeft(imageView));
+                render.start();
+            }
+        });
+
+        buttonRotateInDownRight.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Rotate.InDownRight(imageView));
+                render.start();
+            }
+        });
+
+        buttonRotateInUpLeft.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Rotate.InUpLeft(imageView));
+                render.start();
+            }
+        });
+
+        buttonRotateInUpRight.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Rotate.InUpRight(imageView));
+                render.start();
+            }
+        });
+
+        buttonRotateOutDownLeft.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Rotate.OutDownLeft(imageView));
+                render.start();
+            }
+        });
+
+        buttonRotateOutDownRight.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Rotate.OutDownRight(imageView));
+                render.start();
+            }
+        });
+
+        buttonRotateOutUpLeft.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Rotate.OutUpLeft(imageView));
+                render.start();
+            }
+        });
+
+        buttonRotateOutUpRight.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Rotate.OutUpRight(imageView));
+                render.start();
+            }
+        });
+
+        buttonRotateIn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Rotate.In(imageView));
+                render.start();
+            }
+        });
+
+        buttonRotateOut.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                render.setAnimation(Rotate.Out(imageView));
+                render.start();
+            }
+        });
+
 
     }
 }
