780a11e73d81d9e35042bd6b45fa48c37ace7fc4
gayankuruppu <example@example.com>
2019-09-21 09:05:27 +0530

    Updates

---
+++ precache-manifest.27164c116a5bcb3d976ebd6db2ef56d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67bc12f2ebce2b6cb0e9f1c5ac14dc6",
+    "url": "/android-view-animations-java/index.html"
+  },
+  {
+    "revision": "f2d70ffa317df40dd70b",
+    "url": "/android-view-animations-java/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ae2ca665661c2dedaeab",
+    "url": "/android-view-animations-java/static/js/2.6fb05c39.chunk.js"
+  },
+  {
+    "revision": "f2d70ffa317df40dd70b",
+    "url": "/android-view-animations-java/static/js/main.abada860.chunk.js"
+  },
+  {
+    "revision": "8c9b230d44c03fa5f494",
+    "url": "/android-view-animations-java/static/js/runtime-main.95c7edbd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/android-view-animations-java/static/media/logo.25bf045c.svg"
+  }
+]);
